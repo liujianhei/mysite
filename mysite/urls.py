369275a8e882befbf732a30f00a077ed54907050
Jianhong Liu <liujianhong1986@gmail.com>
2013-06-25 22:49:21 +0800

    Chapter 3: Views and URLconfs

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from mysite.views import hello
+from mysite.views import hello, current_datetime, hours_ahead
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -7,6 +7,8 @@ from mysite.views import hello
 
 urlpatterns = patterns('',
     url(r'^hello/$', hello),
+    url(r'^time/$', current_datetime),
+    url(r'^time/plus/(\d{1,2})/$', hours_ahead),
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^mysite/', include('mysite.foo.urls')),
